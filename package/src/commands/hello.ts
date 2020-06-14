d377bc46303d81a7643bb61faeed419778c211aa
Jacob Evans <cloud887@gmail.com>
2020-06-14 11:29:05 -0500

    Creating Yarn Workspace

--- package/src/commands/hello.ts
+++ package/src/commands/hello.ts
@@ -11,18 +11,18 @@ hello world from ./src/hello.ts!
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    // flag with a value (-O, --name=VALUE)
+    fileName: flags.string({char: 'O', description: 'file name output'}),
+    // flag with no value (-F, --force)
+    force: flags.boolean({char: 'F'}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'fileName'}]
 
   async run() {
     const {args, flags} = this.parse(Hello)
 
-    const name = flags.name ?? 'world'
+    const name = flags.fileName ?? 'world'
     this.log(`hello ${name} from ./src/commands/hello.ts`)
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`)
