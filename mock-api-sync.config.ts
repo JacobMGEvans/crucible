c81750636667e3e05239431dbf81ae5a29c75075
Jacob Evans <cloud887@gmail.com>
2020-07-05 23:36:24 -0500

    Configurations and Base file

--- mock-api-sync.config.ts
+++ mock-api-sync.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   multipleEndpoints: true, // *  Default false
   endpoint: ['/planetary/apod?api_key=DEMO_KEY'], //* Array if multiple is true
-  api: 'https://api.nasa.gov', 
-  dirName: //* Default will be __APIMocks__ (Working Name)
+  baseUrl: 'https://api.nasa.gov', 
+  mockDirectory: '__APIMocks__'//* Default will be __APIMocks__ (Working Name)
 }
